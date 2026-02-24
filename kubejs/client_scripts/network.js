46b928d604f3e44be2a2d2f88b6e27d44dc69e5e
Aero Seira <145478933+Aero-Seira@users.noreply.github.com>
2026-02-24 22:28:23 +0800

    移除tetra部分（不完全）

--- kubejs/client_scripts/network.js
+++ kubejs/client_scripts/network.js
@@ -1,6 +1,6 @@
 
 const $FluidFX = Java.loadClass("com.simibubi.create.content.fluids.FluidFX")
-const $HoloGui = Java.loadClass("se.mickelus.tetra.items.modular.impl.holo.gui.HoloGui")
+
 
 NetworkEvents.dataReceived("kubejs_player_playsound", e => {
     e.player.playSound(e.data.get("soundEvent").getAsString())
@@ -12,11 +12,6 @@ NetworkEvents.dataReceived("spawn_create_particle", e => {
     e.player.level.spawnParticles(particle, true, x, y, z, vx, vy, vz, count, speed)
 })
 
-NetworkEvents.dataReceived("openholo", e => {
-    let gui = $HoloGui.getInstance()
-    Client.setScreen(gui)
-    gui.onShow()
-})
 
 // 发送玩家是否按下了Alt键，用于 批量套壳 server_scripts/Custom/encase.js
 let playerAltDown = false
--- kubejs/server_scripts/Tetra/tag.js
+++
@@ -1,5 +0,0 @@
-ServerEvents.tags("minecraft:block", e => {
-    e.add("tetra:scannable",
-        "ae2:mysterious_cube",
-        "#iceandfire:dragon_environment_blocks")
-})
